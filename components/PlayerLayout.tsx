a872a1eedfb98d65f41d91fb27bfa9ee11a3c8ed
Mohamed <mhmdmrabet@yahoo.fr>
2022-03-17 18:17:49 +0100

    Playlist Container Setup

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -2,9 +2,16 @@ import { Box } from "@chakra-ui/layout";
 
 const PlayerLayout = ({ children }) => {
   return (
-    <Box>
-      Layout Here
-      {children}
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" width="250px" left="0">
+        SIDEBAR
+      </Box>
+      <Box marginBottom="100px" marginLeft="250px">
+        {children}
+      </Box>
+      <Box position="absolute" bottom="0" left="0">
+        PLAYER
+      </Box>
     </Box>
   );
 };
