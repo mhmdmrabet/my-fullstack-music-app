dd2e6216a12b3dda4a3ab16063b67212ed89f227
Mohamed <mhmdmrabet@yahoo.fr>
2022-03-17 17:19:11 +0100

    Creating Player Layout

---
+++ components/PlayerLayout.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/layout";
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box>
+      Layout Here
+      {children}
+    </Box>
+  );
+};
+
+export default PlayerLayout;
