c155c95628005fcd6af537e7f29cf00552e25ddc
Mohamed <mhmdmrabet@yahoo.fr>
2022-03-19 15:36:09 +0100

    Sign In & Sign Up

--- components/AuthForms.tsx
+++ components/AuthForms.tsx
@@ -1,7 +1,8 @@
-import { Box, Flex, Input } from "@chakra-ui/react";
+import { Box, Button, Flex, Input } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { FC, useState } from "react";
 import { useSWRConfig } from "swr";
+import NextImage from "next/image";
 import { auth } from "../lib/mutation";
 
 const AuthForm: FC<{ mode: "signin" | "signup" }> = ({ mode }) => {
@@ -10,13 +11,57 @@ const AuthForm: FC<{ mode: "signin" | "signup" }> = ({ mode }) => {
   const [isLoading, setisLoading] = useState(false);
   const router = useRouter();
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setisLoading(true);
+
+    await auth(mode, { email, password });
+
+    setisLoading(false);
+
+    router.push("/");
+  };
+
   return (
     <Box height="100vh" width="100vw" bg="black" color="white">
-      <Flex justify="center" align="center" height="100px">
-        Selem Imran
+      <Flex
+        justify="center"
+        align="center"
+        height="100px"
+        borderBottom="white 1px solid"
+      >
+        <NextImage src="/logo.svg" height={60} width={120} />
       </Flex>
       <Flex justify="center" align="center" height="calc(100vh - 100px)">
-        Selem Myriam
+        <Box padding="50px" bg="gray.900" borderRadius="6px">
+          <form onSubmit={handleSubmit}>
+            <Input
+              margin={5}
+              placeholder="email"
+              type="email"
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <Input
+              margin={5}
+              placeholder="password"
+              type="password"
+              onChange={(event) => setpassword(event.target.value)}
+            />
+            <Button
+              type="submit"
+              margin={5}
+              bg="green.500"
+              isLoading={isLoading}
+              sx={{
+                "&:hover": {
+                  bg: "green.300",
+                },
+              }}
+            >
+              {mode}
+            </Button>
+          </form>
+        </Box>
       </Flex>
     </Box>
   );
