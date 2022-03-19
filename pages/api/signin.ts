53494e45de3e74a387598559f91827061ea732f8
Mohamed <mhmdmrabet@yahoo.fr>
2022-03-19 14:35:56 +0100

    Signin API Route

---
+++ pages/api/signin.ts
@@ -0,0 +1,40 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma";
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  const { email, password } = request.body;
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        time: Date.now(),
+      },
+      "hello",
+      {
+        expiresIn: "8h",
+      }
+    );
+
+    response.setHeader(
+      "Set-Cookie",
+      cookie.serialize("TRAX_ACCESS_TOKEN", token, {
+        httpOnly: true,
+        maxAge: 8 * 60 * 60,
+        path: "/",
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+      })
+    );
+
+    return response.json(user);
+  }
+  return response.status(401).json({ error: "Invalid credentials" });
+};
