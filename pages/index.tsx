52742b5a5dc0ad3276ba170e31a6a5116d46df9a
Mohamed <mhmdmrabet@yahoo.fr>
2022-03-20 15:37:58 +0100

    Artists List UI Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 import GradientLayout from "../components/GradientLayout";
 import prisma from "../lib/prisma";
 
@@ -11,7 +13,36 @@ const Home = ({ artists }) => {
       image="./me.jpeg"
       roundImage
     >
-      Home Page
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="medium">only visible to tyou</Text>
+        </Box>
+        <Flex gap={5}>
+          {artists.map(({ name, id }) => (
+            <Box
+              bg="gray.900"
+              borderRadius="4px"
+              padding="15px"
+              width="20%"
+              key={id}
+            >
+              <Box width="100%">
+                <Image
+                  src="https://picsum.photos/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
 };
