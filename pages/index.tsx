1a30321ab852aaa8bb665d1647f15a17d455a5bb
Mohamed <mhmdmrabet@yahoo.fr>
2022-03-20 14:54:34 +0100

    Title & Subtitle UI Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,14 @@ import GradientLayout from "../components/GradientLayout";
 
 const Home = () => {
   return (
-    <GradientLayout color="green" roundImage>
+    <GradientLayout
+      color="green"
+      subtitle="Profile"
+      title="M'rabet Imran"
+      description="15 public playlists"
+      image="./me.jpeg"
+      roundImage
+    >
       Home Page
     </GradientLayout>
   );
