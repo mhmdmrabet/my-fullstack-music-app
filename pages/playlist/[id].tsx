857ffc855a91983de252562a20ec11255727c9a4
Mohamed <mhmdmrabet@yahoo.fr>
2022-03-21 11:42:00 +0100

    Playlist Page UI

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,9 +1,36 @@
 import { Box } from "@chakra-ui/layout";
+import GradientLayout from "../../components/GradientLayout";
 import { validateToken } from "../../lib/auth";
 import prisma from "../../lib/prisma";
 
+const getBGColor = (id) => {
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "purple",
+    "orange",
+    "gray",
+    "yellow",
+    "teal",
+  ];
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
+
 const Playlist = ({ playlist }) => {
-  return <Box>{playlist.name} </Box>;
+  const color = getBGColor(playlist.id);
+  return (
+    <GradientLayout
+      color={color}
+      roundImage={false}
+      title={playlist.name}
+      subtitle="playlist"
+      description={`${playlist.songs.length} songs`}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+    >
+      <Box>123</Box>
+    </GradientLayout>
+  );
 };
 
 export const getServerSideProps = async ({ query, req }) => {
